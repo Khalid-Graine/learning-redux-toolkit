d6b8a54cbd4fa86f26597c30fa1b4696f08ed95a
Khalid Graine <khalidgraine1@gmail.com>
2023-11-21 11:40:55 +0100

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,20 @@
-// App.js
-import React, { useState } from 'react';
+
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement , reset} from './store/features/counterSlice';
-import {change} from "./store/features/user"
-import Color from './components/Color';
+import NavBar from './components/NavBar';
 
 
 function App() {
   
-  // const { count, user } = useSelector((state) => (
-  //   console.log(state)
-  // ));
+
   console.log(useSelector((x) => console.log(x.cart)))
-  // const [name,setName] = useState(user);
   // const dispatch = useDispatch();
 
 
   return (
    <div>
-    test
+    <NavBar />
    </div>
-    // <div>
-    //     <Color />
-    //     <br />
-      
-    //   <h1>Counter: {count}</h1>
-    //   <button onClick={() => dispatch(increment())}>Increment</button>
-    //   <button onClick={() => dispatch(reset())}>reset</button>
-    //   <button onClick={() => dispatch(decrement())}>Decrement</button>
-
-    //   <br />
-    //   <h1>{user}</h1>
-    //   <form >
-    //     <input type="text" value={name} onChange={(e) => setName(e.target.value)}  />
-    //   </form>
-    //   <button onClick={() => dispatch(change(name))}>chnage</button>
-    // </div>
   );
 }
 
