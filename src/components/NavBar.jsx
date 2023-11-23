d1b219a72bd06b7772a6bb4371050d2747527727
Khalid Graine <khalidgraine1@gmail.com>
2023-11-23 14:44:03 +0100

    edit logo in navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
-
+import { FaStoreAlt } from "react-icons/fa";
 const NavBar = () => {
   const {amount } = useSelector((s) => s.cart)
   return (
     <div className=" bg-blue-400">
       <div className="container flex justify-between items-center">
-      <p>LOGo</p>
+      <div className="flex gap-2">
+       <FaStoreAlt className="text-2xl" />
+       <span className="">store</span>
+      </div>
       <div className="flex items-center gap-1">
      <span className=" font-bold text-2xl"> {amount}</span>
       <CartIcon />
+
       </div>
       </div>
      
