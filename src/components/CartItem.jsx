264aaae3750e58de25cc76fea382c269ff888b73
Khalid Graine <khalidgraine1@gmail.com>
2023-11-24 10:22:39 +0100

    edit cart item component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -9,8 +9,10 @@ const CartItem = ({ item }) => {
 
   return (
     <li className="flex justify-between  p-2">
-      <div className="flex items-center  gap-4">
-        <div className=" shadow-md p-1 rounded-sm flex items-center justify-center w-20 h-20 overflow-hidden">
+      {/* child 1 */}
+      <div className="flex items-center  gap-1">
+        {/* picture of the product */}
+        <div className="flex items-center justify-center w-20 h-20 overflow-hidden">
           <img
             src={item.img}
             className="max-w-full max-h-full object-center object-cover"
@@ -18,11 +20,12 @@ const CartItem = ({ item }) => {
           />
         </div>
 
+        {/* information about the product */}
         <div className="flex flex-col gap-2 items-start ">
           <h3 className="font-semibold"> {item.title}</h3>
           <div className="text-[#195A1D] flex gap-1 font-bold">
             <p>{item.price}</p>
-          <span>$</span>
+            <span>$</span>
           </div>
           <button
             onClick={() => dispatch(deleteItem(item.id))}
@@ -32,20 +35,20 @@ const CartItem = ({ item }) => {
           </button>
         </div>
       </div>
+
+      {/* child 2 */}
       <div className="flex flex-col items-center justify-between text-2xl">
-        <span
+        <IoChevronUpSharp
           onClick={() => dispatch(increase(item.id))}
-          className="cursor-pointer"
-        >
-          <IoChevronUpSharp />
-        </span>
-        <p className="font-semibold text-2xl"> {item.amount}</p>
-        <span
+          className="cursor-pointer active:text-gray-400"
+        />
+
+        <p className="font-semibold text-2xl">{item.amount}</p>
+
+        <IoChevronDownSharp
           onClick={() => dispatch(decrease(item.id))}
-          className=" cursor-pointer"
-        >
-          <IoChevronDownSharp />
-        </span>
+          className=" cursor-pointer active:text-gray-400"
+        />
       </div>
     </li>
   );
